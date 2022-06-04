e109425b747bb838679749853efd916f5d189553
mubarek78 <shifamubarek@yahoo.com>
2022-06-04 17:37:40 -0600

    assignment completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,20 +4,46 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+
+  // create a new array that incloud food list 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    // console.log(newFood);
+    setFoods(newFoodArray);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+// to delete the food when press on <li>  
+  function handleLiClick(id){
+    const newFoodArray = foods.filter((food) => food.id !== id) // we will use .filtar method 
+    setFoods(newFoodArray)
+  }
+
+  function handleUpdateList(id){
+    
+    // update food list 
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  const foodList = foods.map((food) => {
+    <li key={food.id} onClick={() =>  handleLiClick(food.id)}>
+      {food.name} |  Heat:{food.heatLevel} |  Cuisine: {food.cuisine}
     </li>
-  ));
+  })
 
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
+      <button onClick={handleAddFood}> Add new food</button>
       <ul>{foodList}</ul>
     </div>
   );
